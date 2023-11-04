d67deae9d600c66ea39e5def8481c9b6c48bdb06
John Pavek <JohnQPavek@gmail.com>
2023-11-03 19:32:14 -0500

    Fixes stupid color mode

--- ArtExport.sh
+++ ArtExport.sh
@@ -6,7 +6,7 @@ inputFolder="Assets/Aseprite"
 for file in "$inputFolder"/*.ase; do
 	filename=$(basename "$file")
 	filenameAlone="${filename%.*}"
-	arguments="-b Assets/Aseprite/$filenameAlone.ase --data Assets/Art/$filenameAlone.json --sheet Assets/Art/$filenameAlone.png --sheet-pack --format json-array --filename-format {tag}-{frame} --list-tags --extrude"
+	arguments="-b Assets/Aseprite/$filenameAlone.ase --data Assets/Art/$filenameAlone.json --sheet Assets/Art/$filenameAlone.png --sheet-pack --format json-array --filename-format {tag}-{frame} --list-tags --extrude --color-mode rgb"
 	eval "$asepritePath $arguments"
 done
 
